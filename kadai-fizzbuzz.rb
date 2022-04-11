19ef9d4babc3297cc3c7e69dac3302bae7dfda4c
pac.a.shimizu <pac.a.shimizu@gmail.com>
2022-04-11 14:30:56 +0000

    update kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   else
-    puts num
-  end 
+    return num
+  end
 end
 
 num_max = 100
 (1..num_max).each do |num|
-  fizzbuzz(num)
+  puts fizzbuzz(num)
 end 
